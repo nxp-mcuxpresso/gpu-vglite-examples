8b5900d519dcdad49d827da74670667c8951ffcb
Nilam Gaikwad <nilam.gaikwad@nxp.com>
2024-10-22 11:39:38 +0530

    MGG-1480 [#ccc]: Modify SDK application clock_freertos to use new toolkit graphics assets
    Signed-off-by: Kratika Jain <kratika.jain@nxp.com>

--- example/clock_freertos/clock_freertos.c
+++ example/clock_freertos/clock_freertos.c
@@ -5,34 +5,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-/* FreeRTOS kernel includes. */
-#include "FreeRTOS.h"
-#include "task.h"
-
-#include "fsl_debug_console.h"
-#include "board.h"
-#include "app.h"
-/*-----------------------------------------------------------*/
-#include "vg_lite.h"
-#include "vglite_support.h"
-#include "vglite_window.h"
-#include "Elm.h"
-
-#include "clock_analog.h"
-#include "hour_needle.h"
-#include "minute_needle.h"
-
-/*******************************************************************************
- * Definitions
- ******************************************************************************/
-#define APP_BUFFER_COUNT 2
-#define DEFAULT_SIZE     256.0f;
-
-typedef struct elm_render_buffer
-{
-    ElmBuffer handle;
-    vg_lite_buffer_t *buffer;
-} ElmRenderBuffer;
+#include "layer.h"
 
 /*******************************************************************************
  * Prototypes
@@ -45,29 +18,40 @@ static void vglite_task(void *pvParameters);
 static vg_lite_display_t display;
 static vg_lite_window_t window;
 
-static vg_lite_matrix_t matrix;
-
-static ElmHandle analogClockHandle  = ELM_NULL_HANDLE;
-static ElmHandle hourNeedleHandle   = ELM_NULL_HANDLE;
-static ElmHandle minuteNeedleHandle = ELM_NULL_HANDLE;
-static ElmRenderBuffer elmFB[APP_BUFFER_COUNT];
-
-extern unsigned int ClockAnalog_evo_len;
-extern unsigned char ClockAnalog_evo[];
-
-extern unsigned int HourNeedle_evo_len;
-extern unsigned char HourNeedle_evo[];
-
-extern unsigned int MinuteNeedle_evo_len;
-extern unsigned char MinuteNeedle_evo[];
 /*******************************************************************************
  * Code
  ******************************************************************************/
 
+UILayers_t g_layers[MAX_UI_LAYERS] =
+{
+	{&ClockAnalogOrange, NULL, NULL, &ClockAnalogOrange_gradient_info, ClockAnalogOrange_color_data},
+	{&HourNeedle, NULL, NULL, &HourNeedle_gradient_info, HourNeedle_color_data},
+	{&MinuteNeedle, NULL, NULL, &MinuteNeedle_gradient_info, MinuteNeedle_color_data}
+};
+
+static void BOARD_ResetDisplayMix(void)
+{
+    /*
+     * Reset the displaymix, otherwise during debugging, the
+     * debugger may not reset the display, then the behavior
+     * is not right.
+     */
+    SRC_AssertSliceSoftwareReset(SRC, kSRC_DisplaySlice);
+    while (kSRC_SliceResetInProcess == SRC_GetSliceResetState(SRC, kSRC_DisplaySlice))
+    {
+    }
+}
+
+
 int main(void)
 {
     /* Init board hardware. */
-    BOARD_InitHardware();
+    BOARD_ConfigMPU();
+    BOARD_BootClockRUN();
+    BOARD_ResetDisplayMix();
+    BOARD_InitLpuartPins();
+    BOARD_InitMipiPanelPins();
+    BOARD_InitDebugConsole();
 
     if (xTaskCreate(vglite_task, "vglite_task", configMINIMAL_STACK_SIZE + 200, NULL, configMAX_PRIORITIES - 1, NULL) !=
         pdPASS)
@@ -82,216 +66,83 @@ int main(void)
         ;
 }
 
-static ELM_BUFFER_FORMAT _buffer_format_to_Elm(vg_lite_buffer_format_t format)
-{
-    switch (format)
-    {
-        case VG_LITE_RGB565:
-            return ELM_BUFFER_FORMAT_RGB565;
-            break;
-        case VG_LITE_BGR565:
-            return ELM_BUFFER_FORMAT_BGR565;
-            break;
-        case VG_LITE_BGRX8888:
-            return ELM_BUFFER_FORMAT_BGRX8888;
-            break;
-        default:
-            return ELM_BUFFER_FORMAT_RGBA8888;
-            break;
-    }
-}
-
-static void cleanup(void)
-{
-    vg_lite_close();
-}
-
-static int load_clock()
-{
-    if (ClockAnalog_evo_len != 0)
-    {
-        analogClockHandle = ElmCreateObjectFromData(ELM_OBJECT_TYPE_EGO, (void *)ClockAnalog_evo, ClockAnalog_evo_len);
-    }
-
-    return (analogClockHandle != ELM_NULL_HANDLE);
-}
-
-static int load_hour()
-{
-    if (HourNeedle_evo_len != 0)
-    {
-        hourNeedleHandle = ElmCreateObjectFromData(ELM_OBJECT_TYPE_EGO, (void *)HourNeedle_evo, HourNeedle_evo_len);
-    }
-
-    return (hourNeedleHandle != ELM_NULL_HANDLE);
-}
-
-static int load_minute()
-{
-    if (MinuteNeedle_evo_len != 0)
-    {
-        minuteNeedleHandle =
-            ElmCreateObjectFromData(ELM_OBJECT_TYPE_EGO, (void *)MinuteNeedle_evo, MinuteNeedle_evo_len);
-    }
-
-    return (minuteNeedleHandle != ELM_NULL_HANDLE);
-}
-
-static int load_texture()
-{
-    int ret = 0;
-    ret     = load_clock();
-    if (ret < 0)
-    {
-        PRINTF("load_clock\r\n");
-        return ret;
-    }
-    ret = load_hour();
-    if (ret < 0)
-    {
-        PRINTF("load_hour\r\n");
-        return ret;
-    }
-    ret = load_minute();
-    if (ret < 0)
-    {
-        PRINTF("load_minute\r\n");
-    }
-    return ret;
-}
-
 static vg_lite_error_t init_vg_lite(void)
 {
     vg_lite_error_t error = VG_LITE_SUCCESS;
-    int ret               = 0;
 
     error = VGLITE_CreateDisplay(&display);
     if (error)
     {
-        PRINTF("VGLITE_CreateDisplay failed: VGLITE_CreateDisplay() returned error %d\n", error);
+        PRINTF("VGLITE_CreateDisplay failed: VGLITE_CreateDisplay() returned error %d\r\n", error);
         return error;
     }
     // Initialize the window.
     error = VGLITE_CreateWindow(&display, &window);
     if (error)
     {
-        PRINTF("VGLITE_CreateWindow failed: VGLITE_CreateWindow() returned error %d\n", error);
+        PRINTF("VGLITE_CreateWindow failed: VGLITE_CreateWindow() returned error %d\r\n", error);
         return error;
     }
     // Initialize the draw.
-    ret = ElmInitialize(DEFAULT_VG_LITE_TW_WIDTH, DEFAULT_VG_LITE_TW_HEIGHT);
-    if (!ret)
+    error = vg_lite_init(DEFAULT_VG_LITE_TW_WIDTH, DEFAULT_VG_LITE_TW_HEIGHT);
+    if (error)
     {
-        PRINTF("ElmInitialize failed\n");
-        cleanup();
-        return VG_LITE_OUT_OF_MEMORY;
+        PRINTF("vg_lite engine init failed: vg_lite_init() returned error %d\r\n", error);
+        vg_lite_close();
+        return error;
     }
     // Set GPU command buffer size for this drawing task.
     error = vg_lite_set_command_buffer_size(VG_LITE_COMMAND_BUFFER_SIZE);
     if (error)
     {
-        PRINTF("vg_lite_set_command_buffer_size() returned error %d\n", error);
-        cleanup();
+        PRINTF("vg_lite_set_command_buffer_size() returned error %d\r\n", error);
+        vg_lite_close();
         return error;
     }
 
-    // Setup a scale at center of buffer.
-    vg_lite_identity(&matrix);
-    vg_lite_translate(window.width / 2.0f, window.height / 2.0f, &matrix);
-    // load the texture;
-    ret = load_texture();
-    if (ret < 0)
-    {
-        PRINTF("load_texture error\r\n");
-        return VG_LITE_OUT_OF_MEMORY;
-    }
-
     return error;
-}
-
-static ElmBuffer get_elm_buffer(vg_lite_buffer_t *buffer)
-{
-    for (int i = 0; i < APP_BUFFER_COUNT; i++)
-    {
-        if (elmFB[i].buffer == NULL)
-        {
-            elmFB[i].buffer = buffer;
-            elmFB[i].handle = ElmWrapBuffer(buffer->width, buffer->height, buffer->stride, buffer->memory,
-                                            buffer->address, _buffer_format_to_Elm(buffer->format));
-            vg_lite_clear(buffer, NULL, 0x0);
-            return elmFB[i].handle;
-        }
-        if (elmFB[i].buffer == buffer)
-            return elmFB[i].handle;
-    }
-    return 0;
-}
-static int render(vg_lite_buffer_t *buffer, ElmHandle object)
-{
-    int status                = 0;
-    ElmBuffer elmRenderBuffer = get_elm_buffer(buffer);
-    status                    = ElmDraw(elmRenderBuffer, object);
-    if (!status)
-    {
-        status = -1;
-        return status;
-    }
-    ElmFinish();
-    return status;
-}
-
-static void redraw()
-{
-    int status = 0;
-
-    vg_lite_buffer_t *rt = VGLITE_GetRenderTarget(&window);
-    if (rt == NULL)
-    {
-        PRINTF("vg_lite_get_renderTarget error\r\n");
-        while (1)
-            ;
-    }
-    static float angle = 0;
-
-    // Draw the path using the matrix.
-    status = render(rt, analogClockHandle);
-    if (status == -1)
-    {
-        PRINTF("ELM Render analogClockHandle Failed");
-        return;
-    }
-
-    ElmReset(hourNeedleHandle, ELM_PROP_TRANSFER_BIT);
-    ElmTransfer(hourNeedleHandle, 200.0, 200.0);
-    ElmRotate(hourNeedleHandle, angle);
-
-    status = render(rt, hourNeedleHandle);
-    if (status == -1)
-    {
-        PRINTF("ELM Render hourNeedleHandle Failed");
-    }
-
-    ElmReset(minuteNeedleHandle, ELM_PROP_TRANSFER_BIT);
-    ElmTransfer(minuteNeedleHandle, 200.0, 200.0);
-    ElmRotate(minuteNeedleHandle, -angle);
-
-    status = render(rt, minuteNeedleHandle);
-    if (status == -1)
-    {
-        PRINTF("ELM Render minuteNeedleHandle Failed");
-    }
-
-    angle += 0.5;
-
-    VGLITE_SwapBuffers(&window);
-
-    return;
 }
 
 uint32_t getTime()
 {
     return (uint32_t)(xTaskGetTickCount() * portTICK_PERIOD_MS);
 }
+
+void init() {
+    for (int i = 0; i < MAX_UI_LAYERS; i++) {
+        init_layer(&g_layers[i]);
+    }
+}
+
+void draw_hour_niddle(vg_lite_buffer_t *rt, float angle) {
+    for (int j = 0; j < g_layers[HOUR_NIDDLE].path->path_count; j++) {
+        memcpy(&g_layers[HOUR_NIDDLE].matrix[j].m, &g_layers[HOUR_NIDDLE].path->transform[j * (sizeof(g_layers[HOUR_NIDDLE].matrix[j].m) / sizeof(float))], sizeof(g_layers[HOUR_NIDDLE].matrix[j].m));
+		vg_lite_translate(200, 200, &g_layers[HOUR_NIDDLE].matrix[j]);
+		vg_lite_rotate(angle, &g_layers[HOUR_NIDDLE].matrix[j]);
+		redraw_layer(rt, &g_layers[HOUR_NIDDLE], &g_layers[HOUR_NIDDLE].matrix[j]);
+    }
+}
+
+void draw_minute_niddle(vg_lite_buffer_t *rt, float angle) {
+    for (int j = 0; j < g_layers[MINUTE_NIDDLE].path->path_count; j++) {
+      	memcpy(g_layers[MINUTE_NIDDLE].matrix[j].m, &g_layers[MINUTE_NIDDLE].path->transform[j * (sizeof(g_layers[MINUTE_NIDDLE].matrix[j].m) / sizeof(float))], sizeof(g_layers[MINUTE_NIDDLE].matrix[j].m));
+		vg_lite_translate(200, 200, &g_layers[MINUTE_NIDDLE].matrix[j]);
+		vg_lite_rotate(angle, &g_layers[MINUTE_NIDDLE].matrix[j]);
+		redraw_layer(rt, &g_layers[MINUTE_NIDDLE], &g_layers[MINUTE_NIDDLE].matrix[j]);
+    }
+}
+
+void redraw(vg_lite_buffer_t *rt) {
+	static float angle = 0;
+	for (int j = 0; j < g_layers[ANALAOG_DIAL].path->path_count; j++) {
+		memcpy(&g_layers[ANALAOG_DIAL].matrix[j].m, &g_layers[ANALAOG_DIAL].path->transform[j * (sizeof(g_layers[ANALAOG_DIAL].matrix[j].m) / sizeof(float))], sizeof(g_layers[ANALAOG_DIAL].matrix[j].m));
+		redraw_layer(rt, &g_layers[ANALAOG_DIAL], &g_layers[ANALAOG_DIAL].matrix[j]);
+		draw_hour_niddle(rt, angle);
+		draw_minute_niddle(rt, angle);
+	}
+	angle += 0.5;
+}
+
 
 static void vglite_task(void *pvParameters)
 {
@@ -309,23 +160,39 @@ static void vglite_task(void *pvParameters)
     error = init_vg_lite();
     if (error)
     {
-        PRINTF("init_vg_lite failed: init_vg_lite() returned error %d\n", error);
+        PRINTF("init_vg_lite failed: init_vg_lite() returned error %d\r\n", error);
         while (1)
             ;
     }
 
+    init();
     uint32_t startTime, time, n = 0;
-    startTime = getTime();
-    while (1)
-    {
-        redraw();
-        n++;
-        if (n > 60)
-        {
-            time = getTime() - startTime;
-            PRINTF("%d frames in %d seconds: %d fps\r\n", n, time / 1000, n * 1000 / time);
-            n         = 0;
-            startTime = getTime();
-        }
-    }
+	startTime = getTime();
+	while (1)
+	{
+		vg_lite_buffer_t *rt = VGLITE_GetRenderTarget(&window);
+		if (rt == NULL)
+		{
+			PRINTF("vg_lite_get_renderTarget error\r\n");
+			while (1)
+				;
+		}
+
+		vg_lite_clear(rt, NULL, 0x0);
+		redraw(rt);
+
+		VGLITE_SwapBuffers(&window);
+
+		n++;
+		if (n > 60)
+		{
+			time = getTime() - startTime;
+			PRINTF("%d frames in %d seconds: %d fps\r\n", n, time / 1000, n * 1000 / time);
+			n         = 0;
+			startTime = getTime();
+		}
+	}
+	for (int i = 0; i < MAX_UI_LAYERS; i++) {
+		free_layer(&g_layers[i]);
+	}
 }
