bcf23cc411ca750ea94713246e40aee385352cdd
Harikrushna Parmar <harikrushna.parmar@nxp.com>
2025-05-12 14:32:14 +0530

    MGG-1562 [#ccc] toolkit_freertos: Use xxx_SERIES macros instead of CPU_xxx macros for RT 3 digits
    Align with the updates for RT 4 digits
    
    Signed-off-by: Harikrushna Parmar <harikrushna.parmar@nxp.com>

--- example/toolkit_freertos/toolkit_freertos.c
+++ example/toolkit_freertos/toolkit_freertos.c
@@ -27,7 +27,7 @@
 #include "fsl_gpio.h"
 #include "display_support.h"
 
-#if defined(CPU_MIMXRT798SGFOA_cm33_core0)
+#if defined(MIMXRT798S_cm33_core0_SERIES)
 #include "fsl_lcdif.h"
 #endif
 /*******************************************************************************
@@ -52,7 +52,7 @@ static vg_lite_window_t window;
 #else
 #define VGLITE_COMMAND_BUFFER_SZ (128 * 1024)
 /* On RT595S */
-#if defined(CPU_MIMXRT595SFFOC_cm33) || defined(CPU_MIMXRT798SGFOA_cm33_core0)
+#if defined(MIMXRT595S_cm33_SERIES) || defined(MIMXRT798S_cm33_core0_SERIES)
 #define VGLITE_HEAP_SZ 0x100000 /* 1 MB */
 /* On RT1170 / RT1160 */
 #elif defined(MIMXRT1176_cm7_SERIES) || defined(MIMXRT1175_cm7_SERIES) || \
@@ -66,11 +66,11 @@ static vg_lite_window_t window;
 #if (720 * 1280 == (DEMO_PANEL_WIDTH) * (DEMO_PANEL_HEIGHT))
 #define TW 720
 /* On RT595S */
-#if defined(CPU_MIMXRT595SFFOC_cm33)
+#if defined(MIMXRT595S_cm33_SERIES)
 /* Tessellation window = 720 x 128 */
 #define TH 128
 /* On RT798S */
-#elif defined(CPU_MIMXRT798SGFOA_cm33_core0)
+#elif defined(MIMXRT798S_cm33_core0_SERIES)
 /* Tessellation window = 720 x 640 */
 #define TH 640
 /* On RT1170 / RT1160 */
